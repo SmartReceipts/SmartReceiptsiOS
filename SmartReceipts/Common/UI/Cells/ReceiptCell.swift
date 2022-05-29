4ed992a70b600e938f64e088d89dad468dec67e7
Bogdan Evsenev <evsenev.developer@gmail.com>
2022-05-29 22:20:22 +0300

    [Fixed] app crashing with large PDF in receipts

--- SmartReceipts/Common/UI/Cells/ReceiptCell.swift
+++ SmartReceipts/Common/UI/Cells/ReceiptCell.swift
@@ -73,17 +73,16 @@ class ReceiptCell: SyncableTableCell {
     private func generatePDFIcon(url: URL) -> UIImage? {
         guard let document = CGPDFDocument(url as CFURL) else { return nil }
         guard let page = document.page(at: 1) else { return nil }
-
         let pageRect = page.getBoxRect(.mediaBox)
-        let renderer = UIGraphicsImageRenderer(size: pageRect.size)
+        let scale = imageButton.bounds.width/pageRect.width
+        let renderer = UIGraphicsImageRenderer(size: imageButton.bounds.size)
         let img = renderer.image { ctx in
             UIColor.white.set()
             ctx.fill(pageRect)
-            ctx.cgContext.translateBy(x: 0.0, y: pageRect.size.height)
-            ctx.cgContext.scaleBy(x: 1.0, y: -1.0)
+            ctx.cgContext.translateBy(x: 0.0, y: imageButton.bounds.size.height)
+            ctx.cgContext.scaleBy(x: scale, y: -scale)
             ctx.cgContext.drawPDFPage(page)
         }
-
         return img
     }
 }
